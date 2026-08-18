e272454d465877ddb8481ca8e680caf2b24022f5
otavioaugustoafm <otavioaugustoafm@gmail.com>
2026-08-18 20:38:54 -0300

    erro estupido

--- app-financeiro-frontend/src/components/ModalAporte.tsx
+++ app-financeiro-frontend/src/components/ModalAporte.tsx
@@ -30,7 +30,7 @@ export default function ModalAporte({ isOpen, onClose, onSuccess }: ModalAporteP
       const token = localStorage.getItem("token");
       
       // ATENÇÃO: Cole a sua URL correta do AWS API Gateway aqui de novo!
-      await axios.post("https://fqdj9kncvf.execute-api.us-east-2.amazonaws.com/prod/api/v1/investimentos", {
+      await axios.post("https://fqdj9kncvf.execute-api.us-east-2.amazonaws.com/api/v1/investimentos", {
         ticker: ticker.toUpperCase(),
         categoria,
         tipo, // Agora enviamos se é COMPRA ou VENDA
